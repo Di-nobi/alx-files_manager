d7070420ac0661929cb822f11d35d02cb481d4d2
Dinobi <architectstan01@gmail.com>
2024-02-13 03:07:59 +0100

    Files Manager Project

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -7,16 +7,16 @@ class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
     if (!email) {
-        return res.status(400).send({"error": "Missing email"});
+        return res.status(400).send({error: "Missing email"});
     }
 
     if (!password) {
-        return res.status(400).send({"error": "Missing password"});
+        return res.status(400).send({error: "Missing password"});
     }
 
     const usrMail = await dbClient.db.collection('users').findOne({ email });
     if (usrMail) {
-        return res.status(400).send({"error": "Already exist"});
+        return res.status(400).send({error: "Already exist"});
     }
     const hashPass = sha1(password);
     const newUsr = await dbClient.db.collection('users').insertOne({ email, password: hashPass});
